3434f7aff78a46d0158a44faad618efd58eb230e
unknown <nitiniyer.95@gmail.com>
2024-06-18 17:46:28 +0530

    data ingestion completed

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Disease-Classification-with-DVC
+
+1. Update config.yaml
+2. Update params.yaml
+3. Update entity
+4. Upate configuration manager in src config
+5. Update the components
+6. Upate the pipeline
+7. Update the main.py
+8. Update dvc.yaml
---
+++ main.py
@@ -0,0 +1,11 @@
+from cnnClassifer import logger
+from cnnClassifer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+STAGE_NAME = 'Data Ingestion Stage'
+try:
+        logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>> stage {STAGE_NAME} completed <<<<< \n\nx=======x")
+except Exception as e:
+        logger.exception(e)
+        raise e
